cfec1f3e269456e92478359a53efa6688b17a562
Olysar1 <giorgibarbakadz1@gmail.com>
2023-09-25 09:39:49 +0400

    add first version of fully working app.

--- src/components/AirportsTabPanel.js
+++ src/components/AirportsTabPanel.js
@@ -7,10 +7,11 @@ const AIRPORTS_API_KEY = "2oSJO3D5MMLUsO8cRJ6kNA==wDcNYghgUJZnkWjx";
 
 const AirportsTabPanel = ({ pickedCountry }) => {
   const [airports, setAirports] = useState([]);
+  const [filteredAirports, setFilteredAirports] = useState([]);
+  const [userInput, setUserInput] = useState("");
   const newPickedCountry = pickedCountry.slice(0, 2);
 
-  console.log(newPickedCountry);
-
+  //fetch airoports data
   useEffect(() => {
     fetch(
       `https://api.api-ninjas.com/v1/airports?country=${newPickedCountry}`,
@@ -31,15 +32,43 @@ const AirportsTabPanel = ({ pickedCountry }) => {
       .catch((err) => console.error(`Error: ${err}`));
   }, [newPickedCountry]);
 
+  //filter airports
+  const filterMatch = (original, substring) => {
+    if (!substring) {
+      return original;
+    } else {
+      return original.filter((airport) =>
+        airport.name.toLowerCase().includes(substring.toLowerCase())
+      );
+    }
+  };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (userInput) setFilteredAirports(filterMatch(airports, userInput));
+    }, 500);
+
+    return () => clearTimeout(timeoutId);
+  }, [userInput, airports]);
+
+  //handle user input
+  const handleUserInput = (e) => {
+    setUserInput(e.target.value);
+    console.log(userInput);
+  };
+
   return (
     <Paper sx={{ padding: 2 }}>
       <Typography variant="h4" sx={{ marginBottom: 2 }}>
         Airports
       </Typography>
-      <Input placeholder="Search for airport"></Input>
+      <Input
+        onChange={handleUserInput}
+        placeholder="Search for airport"
+      ></Input>
       <Grid container>
-        {airports &&
-          airports.map((airport) => {
+        {(userInput &&
+          filteredAirports.map((airport) => {
             return (
               <Grid
                 item
@@ -51,7 +80,21 @@ const AirportsTabPanel = ({ pickedCountry }) => {
                 <Typography variant="body1">{`${airport.icao} - ${airport.name} (${airport.city})`}</Typography>
               </Grid>
             );
-          })}
+          })) ||
+          (airports &&
+            airports.map((airport) => {
+              return (
+                <Grid
+                  item
+                  xs={12}
+                  md={6}
+                  key={airport.icao}
+                  sx={{ paddingTop: 2 }}
+                >
+                  <Typography variant="body1">{`${airport.icao} - ${airport.name} (${airport.city})`}</Typography>
+                </Grid>
+              );
+            }))}
       </Grid>
     </Paper>
   );
